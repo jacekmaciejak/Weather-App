c09dc564be15837ee60bc6371e19d53525295e21
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-07-11 19:41:23 +0200

    Structure and components

---
+++ src/components/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "./App.css";
+import Form from "./Form";
+import Result from "./Result";
+
+class App extends Component {
+  state = {
+    value: ""
+  };
+
+  handleInputChange = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Form value={this.state.value} change={this.handleInputChange} />
+        <Result />
+      </div>
+    );
+  }
+}
+
+export default App;

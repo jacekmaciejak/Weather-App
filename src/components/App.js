e0d9770513a0112f3a7106fb527dfa50fdda60e1
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-07-12 17:50:46 +0200

    Added fetch metod

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,15 @@ import Result from "./Result";
 
 class App extends Component {
   state = {
-    value: ""
+    value: "",
+    date: "",
+    city: "",
+    sunrise: "",
+    sunset: "",
+    temp: "",
+    pressure: "",
+    wind: "",
+    err: ""
   };
 
   handleInputChange = e => {
@@ -13,11 +21,33 @@ class App extends Component {
       value: e.target.value
     });
   };
+  handleCitySubmit = e => {
+    e.preventDefault();
+
+    const API = ` http://api.openweathermap.org/data/2.5/weather?q=${
+      this.state.value
+    }&APPID=a88742f1ffb118d512586c071a4a8a8d&units=metric `;
+
+    fetch(API)
+      .then(response => {
+        if (response.ok) {
+          return response;
+        }
+        throw Error("Nie udalo sie");
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err));
+  };
 
   render() {
     return (
       <div className="App">
-        <Form value={this.state.value} change={this.handleInputChange} />
+        <Form
+          value={this.state.value}
+          change={this.handleInputChange}
+          submit={this.handleCitySubmit}
+        />
         <Result />
       </div>
     );

1ab35c90d7744846b4cab4b547fc266cd1d7f945
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-07-16 17:06:04 +0200

    Updates

---
+++ precache-manifest.e27f29755f7d57a96374cc254fae7333.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0e335bd51799e07319b8746e0022eba",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "6f7c812089fcffce46e8",
+    "url": "/Weather-App/static/css/main.340c5215.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/Weather-App/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "6f7c812089fcffce46e8",
+    "url": "/Weather-App/static/js/main.99ac0a64.chunk.js"
+  },
+  {
+    "revision": "89bb695666ed5c38f231",
+    "url": "/Weather-App/static/js/runtime~main.671df5a1.js"
+  }
+]);
